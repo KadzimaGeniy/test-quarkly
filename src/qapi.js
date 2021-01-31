291904d424c89771e67b76f6c35d15562020adc4
KadzimaGeniy <53836021+KadzimaGeniy@users.noreply.github.com>
2021-01-31 23:02:53 +0300

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60170be38954e5001ef12f40",
+				"60170be38954e5001ef12f42"
+			]
+		},
+		"60170be38954e5001ef12f40": {
+			"id": "60170be38954e5001ef12f40",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60170be38954e5001ef12f42": {
+			"id": "60170be38954e5001ef12f42",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
